0f9d2a7a34be5385a037708ea1b863463a8085f5
andreb308 <107200960+andreb308@users.noreply.github.com>
2024-11-19 19:31:49 -0300

    feat(popup): add sparkles effect

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -3,12 +3,13 @@ import { invoke } from "@tauri-apps/api/tauri";
 import { hide } from "tauri-plugin-spotlight-api";
 import "../styles/App.css";
 import { PopupInput } from "./ui/popup-input";
+import { SparklesCore } from "./ui/sparkles";
 
 function App() {
   const placeholders = [
-    "What's the first rule of Fight Club?",
-    "Who is Tyler Durden?",
-    "Where is Andrew Laeddis Hiding?",
+    "Qual o resultado do relatório mais recente de sustentabilidade da empresa?",
+    "Quais são os principais desafios enfrentados pelo setor DESEMPENHO?",
+    "Mostre-me gráficos de XXX no ano de YYYY",
     "Write a Javascript method to reverse a string",
     "How to assemble your own PC?",
   ];
@@ -21,13 +22,33 @@ function App() {
     console.log("submitted");
   };
   return (
-    <div className="flex items-center justify-center gap-6 flex-col w-[800px] h-[200px] bg-pink-0 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100">
-      <img src="https://placehold.co/200x50" alt="" />
+    <div className="flex items-center justify-center flex-col w-[800px] h-[200px] bg-slate-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border-[0.5px] border-gray-800 overflow-hidden bprder">
+      {/* <img src="https://placehold.co/200x50" alt="" /> */}
       <PopupInput
         placeholders={placeholders}
         onChange={handleChange}
         onSubmit={onSubmit}
       />
+      <div className="w-[800px] h-[200px] absolute top-0 left-0 m-0 p-0">
+        {/* Gradients */}
+        {/* <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" /> */}
+
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={130}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+
+        {/* Radial Gradient to prevent sharp edges */}
+        {/* <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div> */}
+      </div>{" "}
     </div>
   );
 }
