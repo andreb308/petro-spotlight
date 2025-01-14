2fa4091e86b509cef12d827659328672f9b8c15a
andreb308 <107200960+andreb308@users.noreply.github.com>
2025-01-14 10:51:28 +0000

    feat(chat): sidebar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,59 +14,13 @@ function App() {
   const [prompt, setPrompt] = useState("");
   const [name, setName] = useState("");
 
-  const handleKeydown = useCallback((event: KeyboardEvent) => {
-    if (!event.ctrlKey || !event.shiftKey) return;
-    if (event.key.toLowerCase() === "o") {
-      void hide();
-    }
-  }, []);
-
-  useEffect(() => {
-    // get url params
-    const urlParams = new URLSearchParams(window.location.search);
-    setPrompt(urlParams.get("prompt") || "");
-    // setTimeout(() => {
-    //   window.location.href = "https://google.com";
-    // }, 3000);
-    window.addEventListener("keydown", handleKeydown);
-    return () => {
-      window.removeEventListener("keydown", handleKeydown);
-    };
-  }, [handleKeydown]);
-
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
-    <div className="flex items-center justify-center flex-col minw-dvw h-dvh">
-      <div className="row">
-        <form
-          autoComplete="off"
-          onSubmit={(e) => {
-            e.preventDefault();
-            void greet();
-          }}
-        >
-          <h1 className="text-3xl font-bold underline text-center mb-8">
-            {prompt}
-          </h1>
-          <input
-            id="greet-input"
-            autoComplete="off"
-            autoCorrect="off"
-            onChange={(e) => {
-              setName(e.currentTarget.value);
-            }}
-            placeholder="Enter a name..."
-            autoFocus
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
-      <p>{greetMsg}</p>
-    </div>
+    
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import Sidebar from "./components/Sidebar";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <SidebarProvider>
+      {/* <div className="flex flex-row minw-dvw h-dvh ">
+      </div> */}
+        <Sidebar />
+        <SidebarTrigger />
+
+        
+    </SidebarProvider>
   </React.StrictMode>
 );
